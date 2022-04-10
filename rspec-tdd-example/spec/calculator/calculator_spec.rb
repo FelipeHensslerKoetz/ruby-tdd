157f9cdfbc846cc157a083f96a721b041bb851ed
Felipe Henssler Koetz <felipehenssler@gmail.com>
2022-04-10 12:26:46 -0300

    refactor: improve test names and add context

--- rspec-tdd-example/spec/calculator/calculator_spec.rb
+++ rspec-tdd-example/spec/calculator/calculator_spec.rb
@@ -3,21 +3,23 @@
 require 'calculator'
 
 describe Calculator do
-  it 'sum method for two positive numbers' do
-    calc = Calculator.new
-    result = calc.sum(5, 7)
-    expect(result).to eq(12)
-  end
+  context '#sum' do
+    it 'with postive numbers' do
+      calc = Calculator.new
+      result = calc.sum(5, 7)
+      expect(result).to eq(12)
+    end
 
-  specify 'sum method for two negative numbers' do
-    calc = Calculator.new
-    result = calc.sum(-5, -10)
-    expect(result).to eq(-15)
-  end
+    it 'with negative numbers' do
+      calc = Calculator.new
+      result = calc.sum(-5, -10)
+      expect(result).to eq(-15)
+    end
 
-  example 'sum method for a positive number and a negative number' do
-    calc = Calculator.new
-    result = calc.sum(-5, 10)
-    expect(result).to eq(5)
+    it 'with a postive and a negative number' do
+      calc = Calculator.new
+      result = calc.sum(-5, 10)
+      expect(result).to eq(5)
+    end
   end
 end
