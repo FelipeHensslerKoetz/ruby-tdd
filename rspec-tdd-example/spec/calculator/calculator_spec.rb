0cb25e6bcda0f18605bef365eefe367a58fd6b1b
Felipe Henssler Koetz <felipehenssler@gmail.com>
2022-04-10 12:31:08 -0300

    refactor: add subject to calculator class spec

--- rspec-tdd-example/spec/calculator/calculator_spec.rb
+++ rspec-tdd-example/spec/calculator/calculator_spec.rb
@@ -3,21 +3,20 @@
 require 'calculator'
 
 describe Calculator do
+  subject(:calc) { described_class.new }
+
   context '#sum' do
     it 'with postive numbers' do
-      calc = Calculator.new
       result = calc.sum(5, 7)
       expect(result).to eq(12)
     end
 
     it 'with negative numbers' do
-      calc = Calculator.new
       result = calc.sum(-5, -10)
       expect(result).to eq(-15)
     end
 
     it 'with a postive and a negative number' do
-      calc = Calculator.new
       result = calc.sum(-5, 10)
       expect(result).to eq(5)
     end
