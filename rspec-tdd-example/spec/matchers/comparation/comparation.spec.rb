e653e0de4e7696b4798f7d94916ecedd2aa605c3
Felipe Henssler Koetz <felipehenssler@gmail.com>
2022-04-10 13:30:09 -0300

    chore: demonstrate compartion rspec matchers

--- rspec-tdd-example/spec/matchers/comparation/comparation.spec.rb
+++ rspec-tdd-example/spec/matchers/comparation/comparation.spec.rb
@@ -1,27 +1,39 @@
 describe 'Comparation Methods' do
-  it '#equal - test if the object is the same' do
-    x = 'ruby'
-    y = 'ruby'
-    expect(x).not_to equal(y)
-    expect(x).to equal(x)
+  it '>' do
+    expect(1).to be > 0
   end
 
-  it '#be - test if the object is the same' do
-    x = 'ruby'
-    y = 'ruby'
-    expect(x).not_to be(y)
-    expect(y).to be(y)
+  it '>=' do
+    expect(1).to be >= 0
   end
 
-  it 'eql - test if the value' do
-    x = 'ruby'
-    y = 'ruby'
-    expect(x).to eql(y)
+  it '<' do
+    expect(0).to be < 1
   end
 
-  it 'eq - test if the value' do
-    x = 'ruby'
-    y = 'ruby'
-    expect(x).to eq(y)
+  it '<=' do
+    expect(0).to be <= 1
+  end
+
+  it 'be_between inclusive' do
+    expect(1).to be_between(1, 10).inclusive
+  end
+
+  it 'be_between exclusive' do
+    expect(1).not_to be_between(1, 10).exclusive
+  end
+
+  it 'match' do
+    expect('name@gmail.com').to match(/..@../)
+  end
+
+  it 'start_with' do
+    expect('my name is...').to start_with('my')
+    expect(['my', 'name', 'is', '...']).to start_with('my')
+  end
+
+  it 'end_with' do
+    expect('my name is...').to end_with('...')
+    expect(['my', 'name', 'is', '...']).to end_with('...')
   end
 end
