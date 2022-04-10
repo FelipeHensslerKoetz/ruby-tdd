78bc248f580eb754453aa2db3820e395bc285fda
Felipe Henssler Koetz <felipehenssler@gmail.com>
2022-04-10 13:39:02 -0300

    chore: demonstrate classes matchers

---
+++ rspec-tdd-example/spec/matchers/classes/classes_spec.rb
@@ -0,0 +1,21 @@
+describe 'Classes Matchers' do
+  it 'be_instance_of' do
+    expect(1).to be_instance_of(Integer)
+  end
+
+  it 'be_kind_of' do
+    expect(1).to be_kind_of(Object)
+  end
+
+  it 'be_a' do
+    expect([]).to be_a(Array)
+  end
+
+  it 'be_an' do
+    expect([]).to be_an(Object)
+  end
+
+  it 'respond_to' do
+    expect([]).to respond_to(:size)
+  end
+end
