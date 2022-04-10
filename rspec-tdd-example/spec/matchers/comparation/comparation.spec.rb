133f6a4fbefb789d663bb10ac2ed79a7811c8b16
Felipe Henssler Koetz <felipehenssler@gmail.com>
2022-04-10 13:07:19 -0300

    chore: demonstrate comparation rspec matchers

---
+++ rspec-tdd-example/spec/matchers/comparation/comparation.spec.rb
@@ -0,0 +1,27 @@
+describe 'Comparation Methods' do
+  it '#equal - test if the object is the same' do
+    x = 'ruby'
+    y = 'ruby'
+    expect(x).not_to equal(y)
+    expect(x).to equal(x)
+  end
+
+  it '#be - test if the object is the same' do
+    x = 'ruby'
+    y = 'ruby'
+    expect(x).not_to be(y)
+    expect(y).to be(y)
+  end
+
+  it 'eql - test if the value' do
+    x = 'ruby'
+    y = 'ruby'
+    expect(x).to eql(y)
+  end
+
+  it 'eq - test if the value' do
+    x = 'ruby'
+    y = 'ruby'
+    expect(x).to eq(y)
+  end
+end
