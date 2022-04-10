199bdf16e6a53e3034b445ebeff7d1355634dd37
Felipe Henssler Koetz <felipehenssler@gmail.com>
2022-04-10 14:25:08 -0300

    chore: demonstrate attributes rspec matchers

---
+++ rspec-tdd-example/spec/matchers/attributes/attributes_spec.rb
@@ -0,0 +1,10 @@
+require 'person'
+
+describe 'Attributes Matchers' do
+  it 'have_attributes' do
+    person = Person.new
+    person.name = 'Felipe'
+    person.age = 23
+    expect(person).to have_attributes(name: starting_with('Felipe'), age: (be >= 18))
+  end
+end
