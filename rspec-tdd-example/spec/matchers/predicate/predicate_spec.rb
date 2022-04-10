cb502e8ce78af09dedc179db633c144b5baad1f7
Felipe Henssler Koetz <felipehenssler@gmail.com>
2022-04-10 14:31:09 -0300

    chore: demonstrate predicate rspec matchers

---
+++ rspec-tdd-example/spec/matchers/predicate/predicate_spec.rb
@@ -0,0 +1,17 @@
+describe 'Predicate matchers' do
+  it 'odd' do
+    expect(1).to be_odd
+  end
+
+  it 'even' do
+    expect(2).to be_even
+  end
+
+  it 'empty' do
+    expect([]).to be_empty
+  end
+
+  it 'nil' do 
+    expect(nil).to be_nil
+  end
+end
