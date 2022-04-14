048da9973512451d33fd66fb9720d7a1814aa3e7
Felipe Henssler Koetz <felipehenssler@gmail.com>
2022-04-14 14:02:55 -0300

    chore: demonstrate array matchers

---
+++ rspec-tdd-example/spec/matchers/arrays/array_spec.rb
@@ -0,0 +1,16 @@
+RSpec.describe Array.new([1, 2, 3]), 'Arrays matchers' do
+  it '#include' do
+    expect(subject).to include(2)
+    expect(subject).to include(2, 1)
+  end
+
+  it '#match_array' do
+    expect(subject).to match_array([3, 2, 1])
+    expect(subject).not_to match_array([3, 2])
+  end
+
+  it '#contain_exactly' do
+    expect(subject).to contain_exactly(3, 2, 1)
+    expect(subject).not_to contain_exactly(1)
+  end
+end
