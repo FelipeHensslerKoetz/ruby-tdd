a90a7fb863f08398295aafbcb4bb28b8b47f13f1
Felipe Henssler Koetz <felipehenssler@gmail.com>
2022-04-15 16:15:21 -0300

    chore: demonstrate custom negated matcher

--- rspec-tdd-example/spec/matchers/arrays/array_spec.rb
+++ rspec-tdd-example/spec/matchers/arrays/array_spec.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
+RSpec::Matchers.define_negated_matcher :be_an_array_excluding, :include
+
 RSpec.describe Array.new([1, 2, 3]), 'Arrays matchers' do
   it '#include' do
     expect(subject).to include(2)
     expect(subject).to include(2, 1)
   end
+
+  it { is_expected.to be_an_array_excluding(4) }
 
   it '#match_array' do
     expect(subject).to match_array([3, 2, 1])
