45f0222a4760a467f9b7b27a80dc1fc108d4706e
Felipe Henssler Koetz <felipehenssler@gmail.com>
2022-04-15 00:33:22 -0300

    chore: demonstrate one line syntax

--- rspec-tdd-example/spec/matchers/ranges/ranges_spec.rb
+++ rspec-tdd-example/spec/matchers/ranges/ranges_spec.rb
@@ -2,7 +2,9 @@
 
 RSpec.describe (1..10), 'Range Matchers' do
   it '#cover' do
-    expect(subject).to cover(8)
+    is_expected.to cover(8)
     expect(subject).not_to cover(11)
   end
+
+  it { is_expected.to cover(1) }
 end
