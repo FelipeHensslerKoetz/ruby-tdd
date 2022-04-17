b2026e261313c5a3d275a5729120ca22026f624b
Felipe Henssler Koetz <felipehenssler@gmail.com>
2022-04-17 12:51:11 -0300

    chore: demonstrate stubs

---
+++ rspec-tdd-example/lib/course.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class Course
+  attr_accessor :name
+
+  def complete?
+    # true or false
+  end
+end
---
+++ rspec-tdd-example/lib/student.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class Student
+  attr_accessor :name, :email
+
+  def has_finished?(course)
+    # should return true or false based on another logic
+  end
+end
