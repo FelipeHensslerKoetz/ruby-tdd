8b1ca7eac4c0dd1069827a89240cdac40cd2d7fa
Felipe Henssler Koetz <felipehenssler@gmail.com>
2022-04-17 15:23:07 -0300

    chore: demonstrate as_null_object

--- rspec-tdd-example/spec/test_double.rb/test_double_rspec.rb
+++ rspec-tdd-example/spec/test_double.rb/test_double_rspec.rb
@@ -10,4 +10,15 @@ RSpec.describe 'Test Double' do
     puts user.password
     puts user.age
   end
+
+  it 'as_null_object' do
+    user = double('User').as_null_object
+    allow(user).to receive_messages(name: 'Jack', password: 'secret')
+    allow(user).to receive(:age).and_return(42)
+
+    puts user.name
+    puts user.password
+    puts user.age
+    puts user.abc # ignored by as_null_object
+  end
 end
